7c42912e31675d35e4f61a4f75df58e25004dadc
Wilson da Silva Goltara <wilson.goltara15@gmail.com>
2021-12-21 18:06:08 -0300

    update atendimentos.js

--- controllers/atendimentos.js
+++ controllers/atendimentos.js
@@ -1,10 +1,14 @@
+const Atendimento = require('../models/atendimentos');
+
 module.exports = (app) => {
     app.get('/atendimentos', (req, res) => {
         res.send("Você está na rota de atendimentos e realizando um GET!");
     });
 
     app.post('/atendimentos',(req, res) => {
-        console.log(req.body);
-        res.send("Você está na rota de atendimentos e realizando um POST!");
+        const atendimento = req.body;
+
+        Atendimento.adiciona(atendimento);
+        res.send("POST Atendimento.!");
     });
 }
